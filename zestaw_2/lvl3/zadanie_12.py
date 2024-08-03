e7e8c2992db7e00e4b7af769bf40feea3d7afd13
Piotr Haruza <phkrakow6@gmail.com>
2024-08-03 23:16:49 +0200

    tak

--- zestaw_2/lvl3/zadanie_12.py
+++ zestaw_2/lvl3/zadanie_12.py
@@ -1,2 +1,25 @@
-def szyfruj_cyfry(liczba):
-    pass
+def zaszyfruj_cyfry_w_liczbie(liczba):
+    zaszyfrowane = []
+    cyfry = 0
+    liczba_pom = liczba
+    while liczba_pom != 0:
+        liczba_pom = liczba_pom // 10
+        cyfry += 1
+    while liczba > 0:
+        if cyfry == 1:
+            if liczba == 9:
+                zaszyfrowane.append(0)
+                zaszyfrowane.append(1)
+        cyfra = liczba % 10
+        if liczba == 9:
+            zaszyfrowane.append(0)
+        else:
+            cyfra += 1
+            zaszyfrowane.append(cyfra)
+        liczba = liczba // 10
+    zaszyfrowane.reverse()
+    for i in zaszyfrowane:
+        print(i, end="")
+
+
+zaszyfruj_cyfry_w_liczbie(156)
