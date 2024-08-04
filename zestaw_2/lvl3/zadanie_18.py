4844f8956fa1626e1b3897bbca361d9ddea58ec9
Piotr Haruza <phkrakow6@gmail.com>
2024-08-04 22:57:58 +0200

    tak

--- zestaw_2/lvl3/zadanie_18.py
+++ zestaw_2/lvl3/zadanie_18.py
@@ -1,3 +1,24 @@
-def znajdz_odwrocone_liczby():
+def odwroc_liczbe(liczba):
     tab = []
-    pass
+    mnoznik = 1
+    nowa_liczba = 0
+    while liczba != 0:
+        cyfra = liczba % 10
+        tab.append(cyfra)
+        liczba = liczba//10
+    tab.reverse()
+    for i in tab:
+        nowa_liczba += i * mnoznik
+        mnoznik *= 10
+    return nowa_liczba
+
+
+def znajdz_odwrocone_liczby():
+    for i in range(1, 100):
+        if i % 10 == 0:
+            continue
+        if odwroc_liczbe(i) * odwroc_liczbe(i) == odwroc_liczbe(i*i):
+            print(i, odwroc_liczbe(i))
+
+
+print(znajdz_odwrocone_liczby())
