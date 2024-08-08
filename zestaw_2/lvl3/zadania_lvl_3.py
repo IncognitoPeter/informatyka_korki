acb97ab23a2f17b0e984d732abc17124deb36eef
IncognitoPeter <phkrakow6@gmail.com>
2024-08-08 12:27:52 +0200

    tako

--- zestaw_2/lvl3/zadania_lvl_3.py
+++ zestaw_2/lvl3/zadania_lvl_3.py
@@ -15,3 +15,45 @@ def sprawdz_czy_jest_szescianem(liczba):
 
 
 # print(sprawdz_czy_jest_szescianem(8))
+
+def znajdz_rozne_cyfry(liczba):
+    zero, jeden, dwa, trzy, cztery, piec = 0, 0, 0, 0, 0, 0
+    szesc, siedem, osiem, dziewiec, rozne_cyfry = 0, 0, 0, 0, 0
+    while liczba != 0:
+        cyfra = liczba % 10
+        if cyfra == 0 and zero == 0:
+            zero += 1
+            rozne_cyfry += 1
+        if cyfra == 1 and jeden == 0:
+            jeden += 1
+            rozne_cyfry += 1
+        if cyfra == 2 and dwa == 0:
+            dwa += 1
+            rozne_cyfry += 1
+        if cyfra == 3 and trzy == 0:
+            trzy += 1
+            rozne_cyfry += 1
+        if cyfra == 4 and cztery == 0:
+            cztery += 1
+            rozne_cyfry += 1
+        if cyfra == 5 and piec == 0:
+            piec += 1
+            rozne_cyfry += 1
+        if cyfra == 6 and szesc == 0:
+            szesc += 1
+            rozne_cyfry += 1
+        if cyfra == 7 and siedem == 0:
+            siedem += 1
+            rozne_cyfry += 1
+        if cyfra == 8 and osiem == 0:
+            osiem += 1
+            rozne_cyfry += 1
+        if cyfra == 9 and dziewiec == 0:
+            dziewiec += 1
+            rozne_cyfry += 1
+        liczba = liczba // 10
+    return rozne_cyfry
+
+
+# print(znajdz_rozne_cyfry(1255))
+
