cd4a6f319873bd4282e89a58b0fd76d93512bfa3
IncognitoPeter <phkrakow6@gmail.com>
2024-08-09 14:20:35 +0200

    tako

--- zestaw_2/lvl3/test.py
+++ zestaw_2/lvl3/test.py
@@ -1,5 +1,11 @@
 def sprawdz_czy_cyfry_tworza_ciag_rosnacy(liczba):
-    pass
+    while liczba != 0:
+        cyfra_1 = liczba % 10
+        liczba = liczba // 10
+        cyfra_2 = liczba % 10
+        if cyfra_1 <= cyfra_2:
+            return False
+    return True
 
 
 print(sprawdz_czy_cyfry_tworza_ciag_rosnacy(123))
