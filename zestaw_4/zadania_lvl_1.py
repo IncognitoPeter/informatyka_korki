fa063d862de14b447a12e6d6c0ec52eed58bf7e7
Piotr Haruza <phkrakow6@gmail.com>
2024-08-11 23:02:35 +0200

    tak

--- zestaw_4/zadania_lvl_1.py
+++ zestaw_4/zadania_lvl_1.py
@@ -9,3 +9,16 @@ def dec2bin(liczba):
     return a
 
 # print(dec2bin(7))
+
+
+def bin2dec(liczba):
+    a = 0
+    mnoznik = 1
+    while liczba != 0:
+        cyfra = liczba % 10
+        a += cyfra * mnoznik
+        liczba = liczba // 10
+        mnoznik *= 2
+    return a
+
+# print(bin2dec(111))
