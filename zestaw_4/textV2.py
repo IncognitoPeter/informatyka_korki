f634476a1799e317b9be686697b41b7f3492efca
IncognitoPeter <phkrakow6@gmail.com>
2024-08-12 19:11:08 +0200

    tako

--- zestaw_4/textV2.py
+++ zestaw_4/textV2.py
@@ -1,14 +1,12 @@
 def print_subnumbers(n, k):
-    # Oblicz długość liczby n
     n_copy = n
     dlugosc = 0
     while n_copy > 0:
         dlugosc += 1
         n_copy //= 10
 
-    # Maskowanie bitowe
     for maska in range(1 << dlugosc):
-        # Liczenie liczby jedynek w masce
+
         licznik_jedynek = 0
         tmp_maska = maska
         while tmp_maska > 0:
@@ -22,7 +20,6 @@ def print_subnumbers(n, k):
             mnoznik = 1
             for i in range(dlugosc):
                 if maska & (1 << i):
-                    # Pobierz odpowiednią cyfrę z n
                     cyfra = tmp_n % 10
                     sub_number = cyfra * mnoznik + sub_number
                     mnoznik *= 10
@@ -30,6 +27,4 @@ def print_subnumbers(n, k):
 
             print(sub_number)
 
-
-# Przykład użycia
 print_subnumbers(12345, 3)
