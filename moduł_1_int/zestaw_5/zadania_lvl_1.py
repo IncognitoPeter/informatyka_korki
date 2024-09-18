c763d8ca78d613344f7686aa9948d73e24755bfe
Piotr Haruza <phkrakow6@gmail.com>
2024-09-18 22:10:01 +0200

    tak

--- moduł_1_int/zestaw_5/zadania_lvl_1.py
+++ moduł_1_int/zestaw_5/zadania_lvl_1.py
@@ -1,31 +1,14 @@
 # zadanie_1
 def sprawdz_czy_jest_potega(liczba):
-    podstawa_parzysta = 2
-    podstawa_nieparzysta = 3
     if liczba == 1:
         return True
-    if liczba % 2 == 0:
-        mnoznik = 1
-        wynik = podstawa_parzysta
-        while wynik <= liczba:
-            if wynik == liczba:
-                print(f'{podstawa_parzysta}^{mnoznik}')
-            mnoznik += 1
-            wynik *= podstawa_parzysta
-    if liczba % 2 != 0:
-        mnoznik = 1
-        wynik = podstawa_nieparzysta
-        while wynik <= liczba:
-            if wynik == liczba:
-                print(f'{podstawa_nieparzysta}^{mnoznik}')
-            mnoznik += 1
-            wynik *= podstawa_nieparzysta
-    for i in range(3, liczba + 1, 2):
+    for i in range(2, liczba + 1):
         mnoznik = 1
         wynik = i
         while wynik <= liczba:
             if wynik == liczba:
                 print(f'{i}^{mnoznik}')
+                return True
             mnoznik += 1
             wynik *= i
     return False
@@ -37,13 +20,15 @@ def sprawdz_czy_jest_potega(liczba):
 
 
 def licz_punkty_kratowe(a, b):
-    for x in range(a, b + 1):
-        y = 1000 / x
-        if 1000 % x == 0:
-            print(x, y)
+    for i in range(a, b + 1):
+        lepsze_y = 1000 // i
+        y = 1
+        while y <= lepsze_y:
+            print(f'({i},{y})')
+            y += 1
 
 
-# print(licz_punkty_kratowe(1,20))
+# print(licz_punkty_kratowe(1,2))
 
 # zadanie_3
 def rozklad_na_czynniki_z_jedna_wspolna(liczba_1, liczba_2):
@@ -73,6 +58,8 @@ def rozklad_na_czynniki_z_jedna_wspolna(liczba_1, liczba_2):
 # print(rozklad_na_czynniki_z_jedna_wspolna(24, 14))
 
 # zadanie_4
+
+
 def wypisz_trojki_pitagorejskie(n):
     for a in range(1, n):
         for b in range(a + 1, n):
@@ -103,14 +90,17 @@ def wypisz_n_cyfrowe(liczba):
         liczba_v1 = liczba_v1 // 10
     return wynik
 
+
 def wypisz_liczby():
-    for i in range(1,100):
+    for i in range(1, 100):
         if wypisz_n_cyfrowe(i):
             print(i)
 
 # print(wypisz_liczby())
 
 # zadanie_6
+
+
 def pierwiastek_calkowitolicznowy():
     pass
 
